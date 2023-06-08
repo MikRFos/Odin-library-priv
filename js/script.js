d41160eaf324fd9048406c14d4b9367e2f17f4e6
MikRFos <mikrfos@gmail.com>
2023-06-08 09:29:52 -0400

    Change to class based approach.
    Added Book and Library classes to create books and a library.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
+/* eslint-disable max-classes-per-file */
 const addBookButton = document.querySelector('.add-book');
 const submitBookButton = document.querySelector('.book-submit');
 const cardContainer = document.querySelector('.card-container');
 const modal = document.querySelector('dialog');
 const bookArray = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.read = read;
-  this.pages = pages;
-}
-
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages 
-          ${this.read ? 'has been read' : 'has not been read'}`;
-};
-
-function addBook(book) {
-  bookArray.push(book);
-  displayBooks();
-}
-
-function removeBook(book) {
-  const index = bookArray.indexOf(book);
-  if (index > -1) {
-    bookArray.splice(index, 1);
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
-  displayBooks();
+
+  get info() {
+    return `${this.title} by ${this.author}, ${this.pages} pages ${this.read ? 'has been read' : 'has not been read'}`;
+  }
 }
+
+class Library {
+  bookList = [];
+  
+  addBook(book) {
+    this.bookList.push(book);
+    displayBooks();
+  }
+
+  removeBook(book) {
+    const index = this.bookList.indexOf(book);
+    if (index > -1) {
+      this.bookList.splice(index, 1);
+    }
+    displayBooks();
+  }
+
+  get getBooks() {
+    return this.bookList;
+  }
+}
+
+const library = new Library();
 
 addBookButton.addEventListener('click', () => {
   modal.showModal();
@@ -37,9 +49,10 @@ submitBookButton.addEventListener('click', () => {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const pages = document.querySelector('#pages').value;
-  const read = document.querySelector('#read').value;
+  const read = document.querySelector('#read').checked;
+  console.log(read);
   const newBook = new Book(title, author, pages, read);
-  addBook(newBook);
+  library.addBook(newBook);
   document.querySelector('.book-input').reset();
   modal.close();
 });
@@ -53,7 +66,7 @@ function createCard(book) {
   title.classList.add('book-title');
 
   const info = document.createElement('p');
-  info.textContent = book.info();
+  info.textContent = book.info;
   info.classList.add('book-info');
 
   const deleteButton = document.createElement('button');
@@ -66,11 +79,11 @@ function createCard(book) {
   cardContainer.appendChild(card);
 
   deleteButton.addEventListener('click', () => {
-    removeBook(book);
+    library.removeBook(book);
   });
 }
 
 function displayBooks() {
   cardContainer.textContent = '';
-  bookArray.forEach(createCard);
+  library.getBooks.forEach(createCard);
 }
